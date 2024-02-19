6abf6d8ab41d63ae3a2d2e89f9655511dd2462f8
Andrey <andrei-cheporov@yandex.ru>
2024-02-19 12:28:13 +0300

    3LAB is ready

---
+++ backend_lab3/backend_lab3/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+var services  = new ServiceCollection();
+services.AddScoped<ILogService, SimpleLogService>();
+services.AddTransient <Logger>();
+
+
+using var serviceProvider = services.BuildServiceProvider();
+
+Logger? logger = serviceProvider.GetService<Logger>();
+logger.Log("Добро пожаловать!");
+
+interface ILogService
+{
+    void Write(string message);
+}
+
+class SimpleLogService : ILogService
+{
+    public SimpleLogService()
+    {
+        Console.WriteLine("Создался сервис");
+    }
+    public void Write(string message) => Console.WriteLine(message);
+}
+
+class RedLogService : ILogService
+{
+    public void Write(string message)
+    {
+        var defaultColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = defaultColor;
+    }
+}
+
+class Logger
+{
+    ILogService? logService;
+    public Logger(ILogService? logService) =>this.logService = logService;
+    public void Log(string message)
+    {
+        logService?.Write($"{DateTime.Now} {message}");
+        logService?.Write($"{DateTime.Now} Проверка выполнения логирования");
+    }
+}
