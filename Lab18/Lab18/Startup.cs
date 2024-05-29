5111e492d264bb536a7556432456abea3006480d
Andrey <andrei-cheporov@yandex.ru>
2024-05-29 23:27:28 +0300

    lab18 added

---
+++ Lab18/Lab18/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Prometheus;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+
+        // Настройка метрик Prometheus
+        app.UseHttpMetrics();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapMetrics(); // Добавляет /metrics endpoint
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
